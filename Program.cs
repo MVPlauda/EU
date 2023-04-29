83a8db4dd2777596464079984f4490c5d53e8463
MVPlauda <plaudadev@gmail.com>
2023-04-28 22:56:26 -0300

    eu

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Qual seu nome?");
+        Console.ReadLine();
+        Console.WriteLine("Qual sua idade?");
+        Console.ReadLine();
+    }
+}
+
+
+
+
+
